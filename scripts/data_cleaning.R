6bf0ca0bcdebc481fe9da42808c631d913530ace
Sebastian Robledo <sebastianrobledo@MacBook-Air-de-Sebastian.local>
2021-05-03 00:45:44 -0500

    added cursos ucla

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -6,8 +6,11 @@ data_cleaning_ucla <- function(grupos_df) {
   trabajos_dirigidos = trabajos_dirigidos_ucla(grupos_df)
   eventos_cientificos = eventos_cientificos_ucla(grupos_df)
   articulos = articulos_ucla(grupos_df)
-  
-  
+  proyectos = proyectos_ucla(grupos_df)
+  capitulos = capitulos_ucla(grupos_df)
+  jurado = jurado_ucla(grupos_df)
+  cursos = cursos_ucla(grupos_df)
+  otros_articulos = otros_articulos_ucla(grupos_df)
   
   
   return(list(trabajos_dirigidos = trabajos_dirigidos,
