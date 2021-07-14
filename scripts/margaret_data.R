10b060dbfbfd9928ac9dc21a6601e1db69c16e06
srobledog <srobledog@unal.edu.co>
2021-07-14 18:53:44 -0500

    removed data_grupos. We'll work with a url added code to export all csv files

--- scripts/margaret_data.R
+++ scripts/margaret_data.R
@@ -13,6 +13,9 @@ source(here("scripts",
 source(here("scripts",
             "data_tidying.R"))
 
+grupos <- read_csv("https://docs.google.com/spreadsheets/d/1gBaXHFp1NTUTeXodb4JyHqY-P-AWV5yN5-p4L1O09gk/export?format=csv&gid=0")
+investigadores <- read_csv("https://docs.google.com/spreadsheets/d/1gBaXHFp1NTUTeXodb4JyHqY-P-AWV5yN5-p4L1O09gk/export?format=csv&gid=347194763")
+
 grupo_df <- data_getting_ucla(grupos)
 produccion_grupos <- data_cleaning_ucla(grupo_df)
 
@@ -21,11 +24,12 @@ source(here("scripts",
 
 articulos_unicos <- data_tidying_ucla(produccion_grupos)
 
-
 produccion_actualizada <- produccion_grupos
 produccion_actualizada[[2]][["articulos"]]<- articulos_unicos
 produccion_actualizada[[2]][["Eliminados_por_grupo"]] <- df_eliminados_total
 produccion_actualizada[[2]][["Similares_entre_grupo"]] <- df_similares_total_grupos
+
+export_csv(produccion_actualizada)
 
 # This code save produccion_grupos in an excel file
 
